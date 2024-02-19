88cbf2d1bde22aaa3c62095b8b026f103a15be72
itnam <mcden@MacBook-Pro-itnam.local>
2024-02-20 00:09:03 +0200

    telegramm

--- tests/telegram.py
+++ tests/telegram.py
@@ -1,7 +1,7 @@
 
 import os
 import requests
-from tests.parsing import GITSpider
+
 def send_message(token, chat_id, message):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
@@ -14,12 +14,8 @@ def send_message(token, chat_id, message):
 # Используйте значения секретов из переменных окружения
 token = os.environ.get('BOT_TOKEN')
 chat_id = os.environ.get('CHAT_ID')
-teleg = GITSpider()
+message = "Результаты тестов: SUCCESS/FAILURE"
 
-for item in teleg.parse(next(teleg.start_urls)):
-    message = f"Результаты тестов: {item}"
-    send_message(token, chat_id, message)
-
-# send_message(token, chat_id, message)
+send_message(token, chat_id, message)
 
 
